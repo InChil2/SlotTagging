74116dd678bbbc1ca4d9c2f7f34d7110ab569825
Mina Park <86277024+parkmina365@users.noreply.github.com>
2021-11-03 22:43:34 +0900

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -17,9 +17,6 @@ class Reader:
         tags_arr = []
         tags_ap = tags_arr.append
         
-        ########################### TODO ###############################
-        # seq.in과 seq.out을 읽어들여서 리스트로 만들기
-        # (텍스트 파일의 한 라인이 리스트의 한 요소가 되도록)
         with open(os.path.join(dataset_folder_path,'seq.in'),'r') as f:
           for sentence in f.readlines():
             text_arr.append(sentence)
@@ -27,7 +24,7 @@ class Reader:
         with open(os.path.join(dataset_folder_path,'seq.out'),'r') as f:
           for sentence in f.readlines():
             tags_arr.append(sentence)
-        ################################################################
 
         assert len(text_arr) == len(tags_arr)
         return text_arr, tags_arr
+    
