b0d151b3dd3201b3780f56eca68307a0e1219d08
Mina Park <86277024+parkmina365@users.noreply.github.com>
2021-11-09 22:45:13 +0900

    Delete code/web_demo directory

--- code/web_demo/README.md
+++
@@ -1,3 +0,0 @@
-# 챗봇 웹 데모  
-  
-https://github.com/Arraxx/new-chatbot 참고  
--- code/web_demo/run.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from app.main import app
-
-if __name__ == "__main__":
-    app.run(port=6006, debug=True)
